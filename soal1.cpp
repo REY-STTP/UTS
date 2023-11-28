949b64d8e7001be40b6f146b053ce8f7f0eae6ff
REY-STTP <rey.zakaria123@gmail.com>
2023-11-28 23:28:43 +0700

    UTS SOAL 1

--- soal1.cpp
+++ soal1.cpp
@@ -2,55 +2,55 @@
 using namespace std;
 
 class PersegiPanjang {
+    // Property
+    public:
+        double panjang;
+        double lebar;
 
-  public:
-    double panjang;
-    double lebar;
+    public:
+        // Konstruktor
+        PersegiPanjang(double panjang = 0, double lebar = 0){
+            PersegiPanjang::panjang = panjang;
+            PersegiPanjang::lebar = lebar;
+        }
 
-  public:
-    // Konstruktor
-    PersegiPanjang(double panjang = 0, double lebar = 0){
-      PersegiPanjang::panjang = panjang;
-      PersegiPanjang::lebar = lebar;
-    }
+        // Method untuk menampilkan informasi persegi panjang
+        void tampilkanInfo() {
+            cout << "Info Persegi Panjang" << endl;
+            cout << "Panjang\t : " << PersegiPanjang::panjang << " cm." << endl;
+            cout << "Lebar\t : " << PersegiPanjang::lebar << " cm." << endl;
+            cout << "Luas\t : " << hitungLuas() << " cm^2." << endl;
+            cout << "Keliling : " << hitungKeliling() << " cm." << endl;
+            cout << "=====================" << endl << endl;
+        }
 
-    // Method untuk menampilkan informasi persegi panjang
-    void tampilkanInfo() {
-        cout << "Info Persegi Panjang" << endl;
-        cout << "Panjang\t : " << PersegiPanjang::panjang << " cm." << endl;
-        cout << "Lebar\t : " << PersegiPanjang::lebar << " cm." << endl;
-        cout << "Luas\t : " << hitungLuas() << " cm^2." << endl;
-        cout << "Keliling : " << hitungKeliling() << " cm." << endl;
-        cout << "=====================" << endl << endl;
-    }
+        // Method untuk menghitung luas
+        double hitungLuas() {
+            return PersegiPanjang::panjang * PersegiPanjang::lebar;
+        }
 
-    // Method untuk menghitung luas
-    double hitungLuas() {
-        return PersegiPanjang::panjang * PersegiPanjang::lebar;
-    }
+        // Method untuk menghitung keliling
+        double hitungKeliling() {
+            return 2 * (PersegiPanjang::panjang + PersegiPanjang::lebar);
+        }
 
-    // Method untuk menghitung keliling
-    double hitungKeliling() {
-        return 2 * (PersegiPanjang::panjang + PersegiPanjang::lebar);
-    }
+        // Method setter
+        void setPanjang(double panjang) {
+            PersegiPanjang::panjang = panjang;
+        }
 
-    // Method setter
-    void setPanjang(double panjang) {
-        PersegiPanjang::panjang = panjang;
-    }
+        void setLebar(double lebar) {
+            PersegiPanjang::lebar = lebar;
+        }
 
-    void setLebar(double lebar) {
-        PersegiPanjang::lebar = lebar;
-    }
+        // Mehtod getter
+        double getPanjang() {
+            return PersegiPanjang::panjang;
+        }
 
-    // Mehtod getter
-    double getPanjang() {
-        return PersegiPanjang::panjang;
-    }
-
-    double getLebar() {
-        return PersegiPanjang::lebar;
-    }
+        double getLebar() {
+            return PersegiPanjang::lebar;
+        }
 };
 
 int main() {
